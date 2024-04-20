f7e50f9577e31159ec8cb016da1d2ddb874fa88d
Alexander Batashev <alexbatashev@outlook.com>
2024-04-20 11:49:19 +0300

    more readable syntax

--- backends/riscv/src/ops/alu.rs
+++ backends/riscv/src/ops/alu.rs
@@ -22,33 +22,40 @@ macro_rules! alu_op_base {
     };
 }
 
-macro_rules! alu_op {
-    ($struct_name:ident, $op_name:literal) => {
+macro_rules! alu_ops {
+    // R-format ALU operations
+    ($($struct_name:ident => { name = $op_name:literal, funct7 = $funct7:literal, funct3 = $funct3:literal })*) => {
+        $(
         alu_op_base!($struct_name, $op_name);
+        )*
+
+        $(
+        impl BinaryEmittable for $struct_name {
+            fn encode(
+                &self,
+                _target_opts: &tir_backend::TargetOptions,
+                _stream: &mut Box<dyn tir_backend::BinaryStream>,
+            ) -> tir_core::Result<()> {
+                Ok(())
+            }
+
+            fn try_decode(_data: &[u8]) -> tir_core::Result<Operation> {
+                todo!()
+            }
+        }
+        )*
     };
 }
 
-alu_op!(AddOp, "add");
-alu_op!(SubOp, "sub");
-alu_op!(SllOp, "sll");
-alu_op!(SltOp, "slt");
-alu_op!(SltuOp, "sltu");
-alu_op!(XorOp, "xor");
-alu_op!(SrlOp, "srl");
-alu_op!(SraOp, "sra");
-alu_op!(OrOp, "or");
-alu_op!(AndOp, "and");
-
-impl BinaryEmittable for AddOp {
-    fn encode(
-        &self,
-        target_opts: &tir_backend::TargetOptions,
-        stream: &mut Box<dyn tir_backend::BinaryStream>,
-    ) -> tir_core::Result<()> {
-        Ok(())
-    }
-
-    fn try_decode(data: &[u8]) -> tir_core::Result<Operation> {
-        todo!()
-    }
+alu_ops! {
+    AddOp => { name = "add", funct7 = 0b0000000, funct3 = 0b000 }
+    SubOp => { name = "sub", funct7 = 0b0100000, funct3 = 0b000 }
+    SllOp => { name = "sll", funct7 = 0b0000000, funct3 = 0b001 }
+    SltOp => { name = "slt", funct7 = 0b0000000, funct3 = 0b010 }
+    SltuOp => { name = "sltu", funct7 = 0b0000000, funct3 = 0b011 }
+    SrlOp => { name = "srl", funct7 = 0b0000000, funct3 = 0b101 }
+    SraOp => { name = "sra", funct7 = 0b0100000, funct3 = 0b101 }
+    OrOp => { name = "or", funct7 = 0b0000000, funct3 = 0b110 }
+    AndOp => { name = "and", funct7 = 0b0000000, funct3 = 0b111 }
 }
+
