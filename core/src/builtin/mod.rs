28343925c60d58845153982382d0941df53343e8
xin0nix <xin0nix@yahoo.com>
2024-04-29 20:19:10 +0300

    feat: basic arith const implementation
    Basic version of arithmentic constant. It is not ready yet, because
    dialect-level type infromation is missing.

--- core/src/builtin/mod.rs
+++ core/src/builtin/mod.rs
@@ -3,10 +3,12 @@ use crate::Op;
 use crate::Operation;
 use crate::Ty;
 
+mod arith;
 mod func;
 mod module;
 mod types;
 
+pub use arith::*;
 pub use func::*;
 pub use module::*;
 use tir_macros::dialect;
@@ -15,5 +17,5 @@ use tir_macros::populate_dialect_types;
 pub use types::*;
 
 dialect!(builtin);
-populate_dialect_ops!(ModuleOp, FuncOp);
+populate_dialect_ops!(ModuleOp, FuncOp, ConstOp);
 populate_dialect_types!(FuncType, VoidType);
