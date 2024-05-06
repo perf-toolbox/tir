0ef3b5fad91b36e3a42e28d3324ebfc92b8b9220
Alexander Batashev <alexbatashev@outlook.com>
2024-05-06 19:22:54 +0300

    just run fmt --check

--- core/src/context.rs
+++ core/src/context.rs
@@ -101,8 +101,7 @@ pub struct Context {
 }
 
 impl Debug for Context {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result 
-    {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str("Context")?;
         Ok(())
     }
