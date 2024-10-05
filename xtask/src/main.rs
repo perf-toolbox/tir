75b83280376ec763d1419b7d06492e2709181570
Alexander Batashev <alexbatashev@outlook.com>
2024-10-05 10:06:09 +0300

    chore: switch to xtask (#90)
    xtask is a more native alternative to cargo-make. While it's not as
    easy to write and understand, it does not require any external setup and
    only uses cargo and rustc to get started. Many Rust projects including
    cargo and rust-analyzer use xtask idiom as an alternative to Makefiles
    and bash scripts.

---
+++ xtask/src/main.rs
@@ -0,0 +1,75 @@
+use std::{env, path::PathBuf};
+use xshell::{cmd, Shell};
+
+fn main() -> anyhow::Result<()> {
+    let task = env::args().nth(1);
+    let sh = Shell::new()?;
+    match task.as_deref() {
+        Some("help") => print_help(),
+        Some("build") => build(&sh)?,
+        Some("check") => {
+            build(&sh)?;
+            check(&sh)?
+        },
+        Some("check-only") => check(&sh)?,
+        Some("docs") => build_docs(&sh)?,
+        _ => print_help(),
+    }
+    Ok(())
+}
+
+fn build(sh: &Shell) -> anyhow::Result<()> {
+    let root = project_root();
+    sh.change_dir(root);
+
+    cmd!(sh, "cargo build").run()?;
+
+    Ok(())
+}
+
+fn check(sh: &Shell) -> anyhow::Result<()> {
+    let root = project_root();
+    sh.change_dir(root);
+
+    cmd!(sh, "cargo run --bin check-runner").run()?;
+
+    Ok(())
+}
+
+fn build_docs(sh: &Shell) -> anyhow::Result<()> {
+    let root = project_root();
+    sh.change_dir(&root);
+
+    cmd!(sh, "cargo doc --no-deps").run()?;
+
+    let api_dest = root.join("docs/api");
+    if let Ok(_) = std::fs::read_dir(&api_dest) {
+        std::fs::remove_dir_all(&api_dest)?;
+    }
+
+    let api_src = root.join("target/doc");
+    std::fs::rename(api_src, api_dest)?;
+
+    cmd!(sh, "mdbook build").run()?;
+
+    Ok(())
+}
+
+fn print_help() {
+    eprintln!(
+        "Tasks:
+
+build            builds TIR project
+check            builds project and runs check tests
+check-only       only runs check tests without building the project
+docs             builds project documentation
+help             shows this message
+"
+    )
+}
+
+fn project_root() -> PathBuf {
+    let dir =
+        env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| env!("CARGO_MANIFEST_DIR").to_owned());
+    PathBuf::from(dir).parent().unwrap().to_owned()
+}
