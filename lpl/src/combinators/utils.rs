6481becfbc1a42d6dd98db5f92e213df9b15afec
Alexander Batashev <alexbatashev@outlook.com>
2024-10-13 14:17:13 +0300

    feat(lpl): implement input isolation
    Add parser combinators to limit consequent parsers' input for easier
    error recovery and better parser resilience.

--- lpl/src/combinators/utils.rs
+++ lpl/src/combinators/utils.rs
@@ -31,3 +31,114 @@ where
             .map(|(output, _)| (output, Some(input)))
     }
 }
+
+pub fn isolate_block<'a, P, SP, EP, Input, Output>(
+    start: SP,
+    end: EP,
+    parser: P,
+) -> impl Parser<'a, Input, Output>
+where
+    Input: ParseStream<'a> + 'a,
+    SP: Parser<'a, Input, ()>,
+    EP: Parser<'a, Input, ()>,
+    P: Parser<'a, Input, Output>,
+{
+    move |input: Input| {
+        let mut depth = 1;
+
+        let mut next_input: Option<Input>;
+
+        match start.parse(input.clone()) {
+            Ok((_, ni)) => {
+                next_input = ni;
+            }
+            Err(err) => return Err(err),
+        }
+
+        while depth > 0 && next_input.is_some() {
+            if let Ok((_, ni)) = start.parse(next_input.as_ref().cloned().unwrap()) {
+                depth += 1;
+                next_input = ni;
+            } else if let Ok((_, ni)) = end.parse(next_input.as_ref().cloned().unwrap()) {
+                depth -= 1;
+                next_input = ni;
+            } else {
+                next_input = next_input.unwrap().slice(1..);
+            }
+        }
+
+        if depth != 0 {
+            return Err(ParserError::new(
+                "failed to match end and start",
+                input.span(),
+            ));
+        }
+
+        let span = input.span();
+
+        let isolated_input = if let Some(ref ni) = next_input {
+            let offset = input.len() - ni.len();
+            input.slice(..offset).unwrap()
+        } else {
+            input
+        };
+
+        let (result, remainder) = parser.parse(isolated_input)?;
+
+        if remainder.map(|r| r.len()).unwrap_or_default() > 0 {
+            return Err(ParserError::new(
+                "nested parser did not consume all of its input",
+                span,
+            ));
+        }
+
+        Ok((result, next_input))
+    }
+}
+
+pub fn isolate_until<'a, P1, P2, Input, Output>(
+    pred: P1,
+    parser: P2,
+) -> impl Parser<'a, Input, Output>
+where
+    Input: ParseStream<'a> + 'a,
+    P1: Parser<'a, Input, ()>,
+    P2: Parser<'a, Input, Output>,
+{
+    move |input: Input| {
+        let mut next_input: Option<Input> = Some(input.clone());
+
+        let mut found = false;
+        while next_input.is_some() {
+            if pred.parse(next_input.as_ref().cloned().unwrap()).is_ok() {
+                found = true;
+                break;
+            }
+
+            next_input = next_input.unwrap().slice(1..);
+        }
+
+        if next_input.is_none() && found == false {
+            return Err(ParserError::new("predicate not found", input.span()));
+        }
+
+        let span = input.span();
+        let isolated = if let Some(ref ni) = next_input {
+            let offset = input.len() - ni.len();
+            input.slice(0..offset).unwrap()
+        } else {
+            input
+        };
+
+        let (result, remainder) = parser.parse(isolated)?;
+
+        if remainder.map(|r| r.len()).unwrap_or_default() > 0 {
+            return Err(ParserError::new(
+                "nested parser did not consume all of its input",
+                span,
+            ));
+        }
+
+        Ok((result, next_input))
+    }
+}
